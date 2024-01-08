ddc37a889cf0115131ba4dfed8462bec55b40702
Chris Fidao <fideloper@gmail.com>
2024-01-08 09:46:44 -0500

    Generate production-ready artifact

--- build.sh
+++ build.sh
@@ -9,13 +9,17 @@ composer install -q --no-interaction --prefer-dist --optimize-autoloader --no-de
 npm ci
 npm run build
 
+# Note that $GITHUB_SHA is set by GitHub Actions
+# For local testing, run:
+# `GITHUB_SHA=$(git rev-parse HEAD) ./build.sh``
+
 # Create a deploy-read artifact
-git archive -o "builds/$(git rev-parse HEAD).zip" HEAD
+git archive -o "builds/$GITHUB_SHA.zip" HEAD
 
 # Add in needed .gitignore'd or .gitattribute export-ignore'd files
-zip -qur "builds/$(git rev-parse HEAD).zip" vendor
+zip -qur "builds/$GITHUB_SHA.zip" vendor
 
 # Uncomment this to include built static assets if they are
 # .gitignore'd (common in team environments)
-## zip -qur "builds/$(git rev-parse HEAD).zip" public/build
+## zip -qur "builds/$GITHUB_SHA.zip" public/build
 
